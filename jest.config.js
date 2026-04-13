cbdbfc27321ff0c43944540aa8c591d366ce94fa
samarthmutagekar23work <smarthmutagekar23work@gmail.com>
2026-04-13 07:17:54 +0530

    add files

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
 module.exports = {
   collectCoverageFrom: [
     'src/context/**/*.{js,jsx}',
-    'src/components/FilterBar/**/*.{js,jsx}',
-    'src/components/Accordian/**/*.{js,jsx}',
-    'src/components/AccContainer/**/*.{js,jsx}',
-    'src/components/Card/**/*.{js,jsx}',
-    'src/components/Cities/**/*.{js,jsx}',
-    'src/components/Collections/**/*.{js,jsx}',
-    'src/components/RestaurantCard/**/*.{js,jsx}',
-    'src/components/SearchBar/**/*.{js,jsx}',
-    'src/components/CTA/**/*.{js,jsx}',
-    'src/components/Footer/**/*.{js,jsx}',
-    'src/data/**/*.{js,jsx}'
+    'src/components/**/*.{js,jsx}',
+    'src/data/**/*.{js,jsx}',
+    'src/pages/**/*.{js,jsx}'
   ],
   coverageThreshold: {
     global: {
