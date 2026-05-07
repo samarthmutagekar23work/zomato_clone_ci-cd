131f19ba9ed71269795bfd68e805b1ac6cf2d3b4
samarthmutagekar23work <smarthmutagekar23work@gmail.com>
2026-05-07 13:15:22 +0530

    Fix Leaflet marker icons missing in Vite build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 // Types
 interface MenuItem {
   id: string;
